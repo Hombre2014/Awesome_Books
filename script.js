27dc066b25dd863fe5972aae69505465a7d98022
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2021-11-22 16:50:18 +0100

    Creare add book function and display it

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
-const title = document.getElementById("title");
-const author = document.getElementById("author");
-const addButton = document.getElementById("add-btn");
-const booksList = document.getElementById("books-list");
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const addButton = document.getElementById('add-btn');
+const booksList = document.getElementById('books-list');
 
-addButton.onclick = function() {
+addButton.onclick = function () {
   const inpTitle = title.value;
   const inpAuthor = author.value;
-
   if (inpTitle && inpAuthor) {
     localStorage.setItem(inpTitle, inpAuthor);
-    // location.reload();
+    location.reload();
   }
+};
 
+function showBooks() {
   for (let i = 0; i < localStorage.length; i += 1) {
     const key = localStorage.key(i);
     const value = localStorage.getItem(key);
     const removeBtn = document.createElement('button');
     const hr = document.createElement('hr');
-    booksList.innerHTML += `${key}<br>${value}<br>`;
-    document.booksList.appendChild(removeBtn);
+    const br = document.createElement('br');
+    const book = document.createElement('div');
+    const bookTitle = document.createElement('h1');
+    const bookAuthor = document.createElement('span');
+    // const bookId = 
+    booksList.appendChild(book);
+    book.appendChild(bookTitle);
+    bookTitle.innerHTML = `${key}<br>`;
+    book.appendChild(bookAuthor);
+    bookAuthor.innerHTML = `${value}<br>`;
+    book.appendChild(removeBtn);
     removeBtn.innerHTML = 'Remove Book';
     removeBtn.className = 'rmv-btn';
-    removeBtn.setAtribute('data-id', (i + 1));
-    document.booksList.appendChild(hr);
+    removeBtn.setAttribute('id', i);
+    book.appendChild(br);
+    book.appendChild(hr);
   }
-  
-};
+}
+
+showBooks();
