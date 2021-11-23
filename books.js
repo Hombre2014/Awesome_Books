633ff10270510c0ac132a6a59777b0c949858a9c
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2021-11-23 08:28:49 +0100

    Modify the add book function

--- books.js
+++ books.js
@@ -3,30 +3,39 @@ const author = document.getElementById('author');
 const addButton = document.getElementById('add-btn');
 const booksList = document.getElementById('books-list');
 
-addButton.onclick = function () {
-  const inpTitle = title.value;
-  const inpAuthor = author.value;
-  if (inpTitle && inpAuthor) {
-    localStorage.setItem(inpTitle, inpAuthor);
+function Book(bookTitle, bookAuthor) {
+  this.bookTitle = bookTitle;
+  this.bookAuthor = bookAuthor;
+}
+
+const newBook = new Book();
+const books = [];
+addButton.onclick = () => {
+  newBook.bookTitle = title.value;
+  newBook.bookAuthor = author.value;
+  if (newBook.bookTitle && newBook.bookAuthor) {
+    localStorage.setItem(newBook.bookTitle, newBook.bookAuthor);
+    books.push(newBook);
+    console.log(books);
     location.reload();
   }
 };
 
-function showBooks() {
+function showBook() {
   for (let i = 0; i < localStorage.length; i += 1) {
-    const key = localStorage.key(i);
-    const value = localStorage.getItem(key);
     const removeBtn = document.createElement('button');
     const hr = document.createElement('hr');
     const br = document.createElement('br');
     const book = document.createElement('div');
-    const bookTitle = document.createElement('h1');
-    const bookAuthor = document.createElement('span');
-    // const bookId = 
+    const bookTitle = document.createElement('p');
+    const bookAuthor = document.createElement('p');
+
     booksList.appendChild(book);
     book.appendChild(bookTitle);
-    bookTitle.innerHTML = `${key}<br>`;
     book.appendChild(bookAuthor);
+    const key = localStorage.key(i);
+    const value = localStorage.getItem(key);
+    bookTitle.innerHTML = `${key}<br>`;
     bookAuthor.innerHTML = `${value}<br>`;
     book.appendChild(removeBtn);
     removeBtn.innerHTML = 'Remove Book';
@@ -37,6 +46,11 @@ function showBooks() {
   }
 }
 
+function removeBook() {
+  const removeButton = document.getElementsByClassName('.rmv-btn');
+  removeButton.onclick = () => {
 
+  }
+}
 
-showBooks();
+showBook();
--- magic.js
+++ magic.js
@@ -3,30 +3,40 @@ const author = document.getElementById('author');
 const addButton = document.getElementById('add-btn');
 const booksList = document.getElementById('books-list');
 
-addButton.onclick = function () {
-  const inpTitle = title.value;
-  const inpAuthor = author.value;
-  if (inpTitle && inpAuthor) {
-    localStorage.setItem(inpTitle, inpAuthor);
+// const book = {
+//   title: this.title,
+//   author: this.author,
+// };
+
+addButton.onclick = () => {
+  const newBook = {};
+  newBook.title = title.value;
+  newBook.author = author.value;
+  if (newBook.title && newBook.author) {
+    localStorage.setItem('book', JSON.stringify(newBook));
+    // localStorage.setItem(newBook.title, newBook.author);
+    console.log(newBook.title, newBook.author);
     location.reload();
   }
 };
 
-function showBooks() {
+function showBook() {
   for (let i = 0; i < localStorage.length; i += 1) {
-    const key = localStorage.key(i);
-    const value = localStorage.getItem(key);
     const removeBtn = document.createElement('button');
     const hr = document.createElement('hr');
     const br = document.createElement('br');
     const book = document.createElement('div');
-    const bookTitle = document.createElement('h1');
-    const bookAuthor = document.createElement('span');
-    // const bookId = 
+    const bookTitle = document.createElement('p');
+    const bookAuthor = document.createElement('p');
+    // const displayBook = [title, author];
+
     booksList.appendChild(book);
     book.appendChild(bookTitle);
-    bookTitle.innerHTML = `${key}<br>`;
     book.appendChild(bookAuthor);
+    const key = localStorage.key(i);
+    const value = localStorage.getItem(key);
+    // const newBook = localStorageArr = JSON.parse(localStorage.getItem('book'));
+    bookTitle.innerHTML = `${key}<br>`;
     bookAuthor.innerHTML = `${value}<br>`;
     book.appendChild(removeBtn);
     removeBtn.innerHTML = 'Remove Book';
@@ -37,6 +47,4 @@ function showBooks() {
   }
 }
 
-
-
-showBooks();
+showBook();
